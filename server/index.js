9b6252ad8ace66edb8d96308d2199d65880cc1dd
effilin <effienummer@gmail.com>
2025-05-02 14:07:44 -0400

    databased connected locally. need to establish routes

---
+++ server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./db");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("database connection failed");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `server is running on port ${PORT} click http://localhost:5000/test-db`
+  );
+});
