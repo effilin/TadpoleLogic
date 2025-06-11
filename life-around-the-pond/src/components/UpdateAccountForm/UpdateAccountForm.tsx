3c74548c733d86cf93e3ef61a48f905631bd2493
effilin <effienummer@gmail.com>
2025-06-10 21:40:41 -0400

    updated account update form to accept jpeg and png

--- life-around-the-pond/src/components/UpdateAccountForm/UpdateAccountForm.tsx
+++ life-around-the-pond/src/components/UpdateAccountForm/UpdateAccountForm.tsx
@@ -1,6 +1,6 @@
 import "./updateAccountForm.css";
 import { useUser } from "../../context/UserContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 type MyProps = {
   onClose: () => void;
 };
@@ -8,10 +8,37 @@ export default function UpdateAccountForm({ onClose }: MyProps) {
   const { user } = useUser();
   const [userName, setName] = useState(user?.username);
   const [profileImg, setProfileImg] = useState(user?.profileImgUrl);
+
+  useEffect(() => {
+    async function handleImageUpdate(e: React.FormEvent) {
+      e.preventDefault();
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_BASE_URL}/api/users`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({}),
+          }
+        );
+        const data = await response.json();
+        if (!response.ok) {
+          console.log(data.error);
+          return;
+        }
+
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [profileImg]);
   return (
     <div className='update-account-container'>
       <button onClick={onClose}>X</button>
-      <form>
+      <form encType='mutipart/form-data'>
         <label>
           Update Username
           <input
@@ -20,14 +47,16 @@ export default function UpdateAccountForm({ onClose }: MyProps) {
             onChange={(e) => setName(e.target.value)}
           />
         </label>
+
         <label>
-          Update Profile Picture
+          Update Picture
           <input
-            type='text'
-            value={profileImg}
-            onChange={(e) => setProfileImg(e.target.value)}
-          />
+            name='image'
+            type='file'
+            accept='image/png, image/jpeg'
+          ></input>
         </label>
+        <button type='submit'> update account </button>
       </form>
     </div>
   );
